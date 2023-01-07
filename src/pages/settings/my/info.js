00df22b70de7e62226241fd7441ec48a6395cd17
Youngwoo <zestlee1106@gmail.com>
2023-01-07 23:15:36 +0900

    [feat] Nested page route 예제 추가
    - Next 는 Nuxt 처럼 페이지 디렉토리 구조로 페이지가 자동으로 routing 된다

---
+++ src/pages/settings/my/info.js
@@ -0,0 +1,14 @@
+import Layout from 'components/Layout'
+import SubLayout from 'components/SubLayout'
+
+export default function MyInfo() {
+  return <h1 className="title">MyInfo</h1>
+}
+
+MyInfo.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  )
+}
